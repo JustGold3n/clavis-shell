49491135912ca4f1f483f21804148d27ff77cebb
JustGold3n <marek@pavlicek.eu>
2026-09-21 09:46:38 +0200

    added quickshell to deps

--- flake.nix
+++ flake.nix
@@ -4,65 +4,70 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+
+    # Quickshell must be pulled in as a flake input as it is not in nixpkgs
+    quickshell.url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
+    quickshell,
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
-        pkgs = import nixpkgs {inherit system;};
+        pkgs = import nixpkgs {
+          inherit system;
+          # Apply quickshell overlay if provided, or append it to the packages
+          overlays = [quickshell.overlays.default];
+        };
       in {
+        # Utilizing qt6.mkDerivation ensures proper QML and Qt plugin paths
         packages.default = pkgs.stdenv.mkDerivation {
           pname = "clavis-shell";
           version = "git-snapshot";
 
-          # Points to the root of the current git repository
-          src = ./.;
+          # cleanSource prevents .git and build/ directories from invalidating the cache
+          src = pkgs.lib.cleanSource ./.;
 
-          # Native build inputs are executed on the build machine
-          nativeBuildInputs = with pkgs; [
-            cmake
-            pkg-config
-            python3
-            qt6.wrapQtAppsHook
-            qt6.qttools
-            wayland-scanner
+          nativeBuildInputs = [
+            pkgs.cmake
+            pkgs.pkg-config
+            pkgs.python3
+            pkgs.qt6.wrapQtAppsHook
+            pkgs.qt6.qttools
+            pkgs.wayland-scanner
+          ];
+          buildInputs = [
+            pkgs.qt6.qtbase
+            pkgs.qt6.qtdeclarative
+            pkgs.qt6.qtwayland
+            pkgs.qt6.qtsvg
+            pkgs.qt6.qt5compat
+            pkgs.qt6Packages.qtkeychain
+            pkgs.wayland
+            pkgs.wayland-protocols
+            pkgs.pam
+            pkgs.networkmanager
+            pkgs.pipewire
+            pkgs.fftw
+            pkgs.libcava
+            pkgs.quickshell
           ];
 
-          # Build inputs are linked against the final binary
-          buildInputs = with pkgs; [
-            qt6.qtbase
-            qt6.qtdeclarative
-            qt6.qtwayland
-            qt6.qtsvg
-            qt6.qt5compat
-            qt6Packages.qtkeychain
-            wayland
-            wayland-protocols
-            pam
-            networkmanager
-            pipewire
-            fftw
-            libcava
-            quickshell
-          ];
-
-          # Standard CMake release flag for optimized, stripped binaries
+          # RPATH stripping is removed; Nix's fixupPhase handles RPATH patching safely.
           cmakeFlags = [
             "-DCMAKE_BUILD_TYPE=Release"
             "-DCMAKE_SKIP_BUILD_RPATH=ON"
           ];
         };
 
-        # Provides a development environment with all dependencies
         devShells.default = pkgs.mkShell {
           inputsFrom = [self.packages.${system}.default];
-          packages = with pkgs; [
-            clang-tools
-            nixpkgs-fmt
+          packages = [
+            pkgs.clang-tools
+            pkgs.nixpkgs-fmt
           ];
         };
       }
